12879182bd29db80b1804da0be4614840d392770
jasmine saad <jasmineysaad@gmail.com>
2021-02-08 18:06:26 -0500

    Second commit, final

---
+++ Bonus13/Bonus13/Class1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bonus13
+{
+    public class GameCharacter
+    {
+        public GameCharacter(int intel, int strength, string name)
+        {
+
+            Intel = intel;
+            Strength = strength;
+            Name = name;
+
+        }
+        public int Intel { get; }
+        public int Strength { get; }
+        public string Name { get; }
+
+        virtual public string Play()
+        {
+
+            return "Player " + Name + ": has strength level: " + Strength + ": has intelligence level: " + Intel;
+        }
+    }
+
+
+
+
+    public class MagicUsingCharacter : GameCharacter
+    {
+        public MagicUsingCharacter(int intel, int strength, string name, int magicalEnergy) : base(intel, strength, name)
+        {
+            MagicalEnergy = magicalEnergy;
+        }
+        public int MagicalEnergy { get; set; }
+        public override string Play()
+        {
+            return base.Play() + " has magic energy: " + MagicalEnergy;
+        }
+
+    }
+
+    public class Wizard : MagicUsingCharacter
+    { 
+        public Wizard(int intel, int strength, string name, int magicalEnergy, int spellnumber):base( intel, strength, name, magicalEnergy)
+        {
+            SpellNumber = spellnumber;
+        }
+        public int SpellNumber { get; set; }
+        public override string Play()
+        {
+            return base.Play() + " spell number:  " + SpellNumber;
+        }
+
+    }
+
+    public class Warrior : GameCharacter
+    {
+        public Warrior(int intel, int strength, string name, string weapontype) : base(intel, strength, name)
+        {
+            WeaponType = weapontype;
+        }
+        public string WeaponType { get; set; }
+        public override string Play()
+        {
+            return base.Play() + " weapon type: " + WeaponType ;
+        }
+
+    }
+}
+
---
+++ Bonus13/Bonus13/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bonus13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameCharacter[] gameCharacters= new GameCharacter[5];
+
+            Console.Write("Enter player name: ");
+            var name = Console.ReadLine();
+            Console.Write("Enter strength from 1-10: ");
+            var strength = int.TryParse(Console.ReadLine(), out var resultstrength) ? resultstrength : default;
+            Console.Write("Enter intelligence from 1-10: ");
+            var intel = int.TryParse(Console.ReadLine(), out var resultintel) ? resultintel : default;
+
+
+            gameCharacters[0] = new Wizard(intel, strength, name, 7, 7);
+            gameCharacters[1] = new Wizard(intel, strength, name, 8, 2);
+            gameCharacters[2] = new Wizard(intel, strength, name, 3, 7);
+            gameCharacters[3] = new Warrior(intel, strength, name, "bow and arrow");
+            gameCharacters[4] = new Warrior(intel, strength, name, "true love");
+
+         
+            
+            foreach (var character in gameCharacters)
+            {
+                Console.WriteLine(character.Play());
+            }
+        }
+    }
+}
